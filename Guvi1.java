fb5fb05afe70589838b4ed656401bc898d5a7c7d
Surya kala <surya.kalacse@ksrce.ac.in>
2016-07-21 11:11:24 +0530

    Create Guvi1.java

---
+++ Guvi1.java
@@ -0,0 +1,16 @@
+public class Guvi1 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+System.out.println("Enter the year");
+int year=s.nextInt();
+if(year%4==0)
+{
+	System.out.println("leep year");
+}
+else
+{
+	System.out.println("not a leep year");
+}
+	}
+
+}
