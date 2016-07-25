3a28452980277c0435bae2414253f28dfce38870
Surya kala <surya.kalacse@ksrce.ac.in>
2016-07-25 09:40:26 +0530

    Create Fact1.java

---
+++ Fact1.java
@@ -0,0 +1,13 @@
+public class Fact1 {
+ public static void main(String[] args) {
+ 	int level;
+ 	long k=1;
+ 	Scanner s=new Scanner(System.in);
+ 	limit=s.nextInt();
+ 	for(int i=1;i<=level;i++)
+ 	{
+ 		 k=k*i;
+ 	}
+ 	System.err.println(k);
+ }
+ }
